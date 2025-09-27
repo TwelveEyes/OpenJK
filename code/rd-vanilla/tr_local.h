078070e2540e3ca9a9a6b7e13ba4ad605e8a4f93
Gene <elkaufman5@gmail.com>
2025-09-26 18:15:48 -0700

    widescreen

--- code/rd-vanilla/tr_local.h
+++ code/rd-vanilla/tr_local.h
@@ -1744,7 +1744,7 @@ typedef struct {
 	float	w, h;
 	float	s1, t1;
 	float	s2, t2;
-	float	a;
+	float	a, ratio;
 } rotatePicCommand_t;
 
 typedef struct
@@ -1810,9 +1810,9 @@ void RE_SetColor( const float *rgba );
 void RE_StretchPic ( float x, float y, float w, float h,
 					  float s1, float t1, float s2, float t2, qhandle_t hShader );
 void RE_RotatePic ( float x, float y, float w, float h,
-					  float s1, float t1, float s2, float t2,float a, qhandle_t hShader );
+					  float s1, float t1, float s2, float t2,float a, qhandle_t hShader, float aspectCorrection );
 void RE_RotatePic2 ( float x, float y, float w, float h,
-					  float s1, float t1, float s2, float t2,float a, qhandle_t hShader );
+					  float s1, float t1, float s2, float t2,float a, qhandle_t hShader, float aspectCorrection );
 void RE_RenderWorldEffects(void);
 void RE_LAGoggles( void );
 void RE_Scissor ( float x, float y, float w, float h);
